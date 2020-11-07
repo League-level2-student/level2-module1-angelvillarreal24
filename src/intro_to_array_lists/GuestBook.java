dae3dc1023d76df5171de84894877d29aceaac5e
angelvillarreal24 <angel.villarreal.24@bishops.com>
2020-11-07 11:28:20 -0800

    Finished GuestBook, Started IntroToArrayLists
    yes

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,39 +1,69 @@
 package intro_to_array_lists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
-	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
-	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
+public class GuestBook implements ActionListener {
+	// Create a GUI with two buttons. One button reads "Add Name" and the other
+	// button reads "View Names".
+	// When the add name button is clicked, display an input dialog that asks the
+	// user to enter a name. Add
+	// that name to an ArrayList. When the "View Names" button is clicked, display a
+	// message dialog that displays
 	// all the names added to the list. Format the list as follows:
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
-	
-	
-	
+
+	// standard global vars
+	JButton addName = new JButton();
+	JButton viewName = new JButton();
+
+	ArrayList<String> nameList = new ArrayList<String>();
+
 	public static void main(String[] args) {
 		GuestBook book = new GuestBook();
 		book.createUI();
-	}
+	}// close main
 
 	private void createUI() {
-		
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
-		JButton addName = new JButton();
-		JButton viewName = new JButton();
-		
+
 		frame.add(panel);
 		panel.add(addName);
 		panel.add(viewName);
-		
+
+		addName.setText("Add Name");
+		viewName.setText("View Names");
+		addName.addActionListener(this);
+		viewName.addActionListener(this);
+
 		frame.setVisible(true);
-		frame.pack();+		
-	}
+		frame.pack();
+	}// close createUI
 	
-}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == addName) {
+			String nameToAdd = JOptionPane.showInputDialog("What name should be added?");
+			nameList.add(nameToAdd);
+			JOptionPane.showMessageDialog(null, "The name " + nameToAdd + " was added!");
+		} else if (e.getSource() == viewName) {
+			String allGuests = "";
+			int counter = 0;
+			for (String s : nameList) {
+				counter++;
+				allGuests += "Guest #" + counter + ": " + s + "\n";
+			}
+			JOptionPane.showMessageDialog(null, allGuests);
+		}
+	}// close actionPerformed
+}// close Guestbook (the class)
--- src/intro_to_array_lists/IntroToArrayLists.java
+++ src/intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,30 @@
 package intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
-		//   Don't forget to import the ArrayList class
-		
-		//2. Add five Strings to your list
-		
-		//3. Print all the Strings using a standard for-loop
-		
-		//4. Print all the Strings using a for-each loop
-		
-		//5. Print only the even numbered elements in the list.
-		
+		//   Don't forget to import the ArrayList class		
+		//2. Add five Strings to your list		
+		//3. Print all the Strings using a standard for-loop		
+		//4. Print all the Strings using a for-each loop		
+		//5. Print only the even numbered elements in the list.		
 		//6. Print all the Strings in reverse order.
-		
 		//7. Print only the Strings that have the letter 'e' in them.
+		
+		ArrayList<String> stringList = new ArrayList<String>();
+		stringList.add("Hello");
+		stringList.add("How");
+		stringList.add("Are");
+		stringList.add("You");
+		stringList.add("Today?");
+		
+		for(int i = 0; i < stringList.size(); i++){
+			String s = stringList.get(i);
+			System.out.println(s);
+		}
+		
+		
 	}
 }
